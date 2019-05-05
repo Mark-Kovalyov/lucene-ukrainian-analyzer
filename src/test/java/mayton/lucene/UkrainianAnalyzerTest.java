14ef86e5278ea48f806c3ebfeaae6e212c8772ec
mayton <mark.kovalyov78@gmail.com>
2019-05-05 21:15:28 +0300

    Add first implementations

---
+++ src/test/java/mayton/lucene/UkrainianAnalyzerTest.java
@@ -0,0 +1,13 @@
+package mayton.lucene;
+
+import org.junit.Test;
+
+public class UkrainianAnalyzerTest {
+
+    @Test
+    public void test() {
+        UkrainianAnalyzer ukrainianAnalyzer = new UkrainianAnalyzer();
+
+    }
+
+}
